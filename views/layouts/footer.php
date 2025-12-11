abec90f01f85c4c752586aaa371906c20bbde4b7
Luclvan <ohyeah97gaming@gmail.com>
2025-12-11 14:25:47 +0700

    login/register/account + basic frontend

---
+++ views/layouts/footer.php
@@ -0,0 +1,10 @@
+    </div> <!-- /.container -->
+</main>
+
+<footer class="main-footer">
+    <div class="container footer-inner">
+        <p>© 2025 OnlineCourse - K65 CSE485</p>
+    </div>
+</footer>
+</body>
+</html>
